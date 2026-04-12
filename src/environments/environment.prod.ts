647f3ca2feb3d8ecc8587485ddbbfa1b30ba3da8
Abanoub Nabil <AbanoubNabil120@outlook.com>
2026-04-12 13:12:33 +0200

    feat: initialize environment configuration files with production and development API URLs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:5298//api'
+  apiUrl: 'https://zadana.runasp.net/api'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://zadana.runasp.net/api'
+  apiUrl: 'http://localhost:5298/api'
 };
